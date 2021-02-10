f896957eb15c1145e44b6a96041d96ac3d432345
david-shakaya <devidoz666@gmail.com>
2021-02-10 13:23:26 +0200

    +

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import ReactDOM from 'react-dom'
 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 
+/* Создаем елемент виртуального дома */
+// 1) а - тип тега
+// 2)href - это обьект настроек.ПРОПС 
+// 3) - Это дети. Текст созданной ссылки
+const link = React.createElement('a', { href: 'https://www.google.com/', target:'_blank' }, 'Ссылка гугл')
+console.log(link);
+
+const root = document.querySelector('#root');
+// Добавляем ел в настоящий дом
+ReactDOM.render(link, root)
+
+
+/* 
+* - Создаем тот же линк но с помощю JSX тега
+ */
+
+const jsxLink = <a className="link"
+  href="'https://www.google.com/'
+   target='_blank'">Ссылка на гугл с помощью JSX</a>
+
+   ReactDOM.render(jsxLink, document.querySelector('#root'))
